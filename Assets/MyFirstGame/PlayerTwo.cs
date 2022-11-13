6404089fbdf55a1339ed7fa6b71d9152ccef8a93
KertiBandi <kerti.andras88@gmail.com>
2022-11-13 21:49:42 +0100

    11.13

--- Assets/MyFirstGame/PlayerTwo.cs
+++ Assets/MyFirstGame/PlayerTwo.cs
@@ -4,6 +4,7 @@ public class PlayerTwo : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float turnSpeed;
+    [SerializeField] float jumpHeight;
 
     void Update()
     {
@@ -13,58 +14,89 @@ public class PlayerTwo : MonoBehaviour
         bool isDownPressed = Input.GetKey(KeyCode.S);
         bool isSpacePressed = Input.GetKey(KeyCode.Space);
 
-        float x = 0;
-        if (isRightPressed)
+
+       
+
+
+         if (isRightPressed)
+         {
+             transform.Rotate(Vector3.up, (turnSpeed * Time.deltaTime));
+         }
+
+         if (isLeftPressed)
+         {
+             transform.Rotate(Vector3.up, (-turnSpeed * Time.deltaTime));
+         }
+
+
+        
+        //Quaternion q = transform.rotation;
+        Vector3 direction = transform.forward;
+
+     
+
+        //Vector3 newForward = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+        if (isUpPressed)
         {
-            x = turnSpeed;
+
+            transform.position += direction * speed * Time.deltaTime;
         }
-        if (isLeftPressed)
+        if (isDownPressed)
         {
-            x =-turnSpeed;
+            transform.position -= direction * speed * Time.deltaTime;
         }
 
-        float z = isUpPressed ? 1 : (isDownPressed ? -1 : 0);
 
-
-
-
-        float y = 0;
         if (isSpacePressed)
         {
-            y += 1;
-
+            GetComponent<Rigidbody>().velocity = new Vector3(0, jumpHeight*Time.deltaTime, 0);
         }
 
-       //transform.Rotate(Vector3.right, (turnSpeed*Time.deltaTime));
-
-
-      /*  Vector3 direction = new Vector3(x, 0, z);
-        direction.Normalize(); // keresztbe mozgás sebességét normalizáltuk
-
-
-
-        Vector3 velocity = direction * speed;
+       // float z = isUpPressed ? 1 : (isDownPressed ? -1 : 0);
 
 
 
 
+        /*  float y = 0;
+          if (isSpacePressed)
+          {
+              y += 1;
+
+          }*/
 
 
-        transform.position += velocity * Time.deltaTime; // time.deltaTime kell hogy egyenletes legyen a mozgás minden gépen
-
-        // egyenletes változás elérésére használjuk a time,deltatime-ot
 
 
-        if (direction != Vector3.zero)
-
-            transform.rotation = Quaternion.LookRotation(direction);
 
 
-        Vector3 jump = new Vector3(0, y, 0);
-        Vector3 jumpSpeed = jump * speed;
+        /*  Vector3 direction = new Vector3(x, 0, z);
+          direction.Normalize(); // keresztbe mozgás sebességét normalizáltuk
 
-        transform.position += jumpSpeed * Time.deltaTime; 
-      */
+
+
+          Vector3 velocity = direction * speed;
+
+
+
+
+
+
+          transform.position += velocity * Time.deltaTime; // time.deltaTime kell hogy egyenletes legyen a mozgás minden gépen
+
+          // egyenletes változás elérésére használjuk a time,deltatime-ot
+
+
+          if (direction != Vector3.zero)
+
+              transform.rotation = Quaternion.LookRotation(direction);
+
+
+          Vector3 jump = new Vector3(0, y, 0);
+          Vector3 jumpSpeed = jump * speed;
+
+          transform.position += jumpSpeed * Time.deltaTime; 
+        */
 
     }
 }
